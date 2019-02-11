17c58b31fc3508eaf51954014f67830e891cd9ad
Szeba25 <lustmord20@gmail.com>
2019-02-11 21:45:14 +0100

    Added relative JRE

--- core/windows_launcher.bat
+++ core/windows_launcher.bat
@@ -1,10 +1,12 @@
+:: New:
 :: Wyvern launcher .bat version.
 @echo off
-::set "RUNTIME_PATH=core files/jre1.8.0_51/bin/java.exe"
-::echo Starting: %RUNTIME_PATH%
-::"%RUNTIME_PATH%" -jar Wyvern.jar
+set "RUNTIME_PATH=core files/jre1.8.0_191/bin/java.exe"
+echo Starting: %RUNTIME_PATH%
+"%RUNTIME_PATH%" -jar Wyvern.jar
 
-java -jar Wyvern.jar
+:: Old:
+:: java -jar Wyvern.jar
+:: pause
 
-::pause
 exit
