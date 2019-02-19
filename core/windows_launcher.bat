0b2438cd2f9ccac5a5ec391310448dd0a92baeb0
Szeba25 <lustmord20@gmail.com>
2019-02-19 14:17:19 +0100

    Updated launcher script

--- core/windows_launcher.bat
+++ core/windows_launcher.bat
@@ -1,7 +1,7 @@
 :: New:
 :: Wyvern launcher .bat version.
 @echo off
-set "RUNTIME_PATH=core files/jre1.8.0_191/bin/java.exe"
+set "RUNTIME_PATH=core files/jre1.8.0_201/bin/java.exe"
 echo Starting: %RUNTIME_PATH%
 "%RUNTIME_PATH%" -jar Wyvern.jar
 
