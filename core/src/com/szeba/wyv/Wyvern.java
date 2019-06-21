7d2d6cdff54cf769e2f6aa910217e818ce8d0a8e
Szeba25 <lustmord20@gmail.com>
2019-06-21 16:43:14 +0200

    Version 821

--- core/src/com/szeba/wyv/Wyvern.java
+++ core/src/com/szeba/wyv/Wyvern.java
@@ -41,7 +41,7 @@ import com.szeba.wyv.widgets.ext.Warning;
 public class Wyvern implements ApplicationListener {
 	
 	// The current version number
-	public static final String VERSION = "v0.820";
+	public static final String VERSION = "v0.821";
 	
 	// The current working directory
 	public static String DIRECTORY;
